19666d44ca4d381900b5f8ed38d8bf75bb8b6634
FHallengreen <fred70p9@stud.kea.dk>
2022-05-04 19:40:56 +0200

    added ok when getting exception error

--- src/Main.java
+++ src/Main.java
@@ -36,14 +36,16 @@ public class Main {
     public void error(NumberFormatException e){
         JFrame frame2 = new JFrame("Oopps you messed up!");
         frame2.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        frame2.setSize(200, 100);
+        frame2.setSize(320, 100);
         frame2.setLocationRelativeTo(null);
         JLabel label3 = new JLabel("ERROR 404! Do something else");
         JPanel panel3 = new JPanel();
         frame2.add(panel3);
         panel3.add(label3);
+        JButton button2 = new JButton("OK");
+        panel3.add(button2);
         frame2.setVisible(true);
-        System.out.println(e);
+        button2.addActionListener(f -> frame2.setVisible(false));
     }
 
 
@@ -53,7 +55,7 @@ public class Main {
         JLabel label = new JLabel("DKK");
         JLabel label2 = new JLabel("USD");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(450, 200);
+        frame.setSize(450, 100);
         frame.setLocationRelativeTo(null);
         frame.add(panel = new JPanel());
 
